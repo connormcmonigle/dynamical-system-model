424fc852f4780c910502ee1f6031a379d22a756c
connormcmonigle <connormcmonigle@gmail.com>
2020-02-11 20:08:27 -0800

    Improved training code

--- inference.cc
+++ inference.cc
@@ -6,16 +6,18 @@
 
 
 int main(){
-    std::fstream load_file("check_pt/model_save_48000.txt");
-    using M = dyn::model<util::info<double, 2, 2, 16>>;
+    std::fstream load_file("check_pt/model_save_3966000.txt");
+    using M = dyn::model<util::info<double, 2, 2, 6>>;
     M m{0.01}; load_file >> m;
     std::cout << m << std::endl;
     M::info::latent_vec_t latent{}; latent.setZero();
     M::info::in_vec_t input{}; input(0) = input(1) = 0.5;
-    for(size_t i(0); i < 2000; ++i){
+    std::fstream csv_output("output.csv", std::ios::app);
+    csv_output << "x, y" << std::endl;
+    for(size_t i(0); i < 10000; ++i){
         const auto [next_input, next_latent] = m.forward(M::backward_t(input, latent));
         input = next_input;
         latent = next_latent;
-        std::cout << input(1) << std::endl;
+        csv_output << input(0) << ", " << input(1) << std::endl;
     }
 }
--- test_train.cc
+++ test_train.cc
@@ -6,17 +6,23 @@
 
 int main(){
     auto data = van_der_pol::data_generator(van_der_pol::config{0.2, 0.01, 5000ull});
-    auto model = dyn::model<util::info<double, 2, 2, 16>>::random(0.0);
+    auto model = dyn::model<util::info<double, 2, 2, 6>>::random(0.0);
     auto trainer = train::trainer(model, data).set_lr(0.001);
-    constexpr int sample_rate = 1000;
+    constexpr int sample_rate = 100;
+    constexpr int save_rate = 6000;
     double sum{0.0};
     for(size_t i{0};;++i){
       sum += trainer.update_model();
+
       if(i !=0 && i % sample_rate == 0){
-        std::cout << sum / static_cast<decltype(sum)>(sample_rate) << std::endl;
+        std::cout << "\r" << std::flush << sum / static_cast<decltype(sum)>(sample_rate);
         sum = 0.0;
-        //std::fstream save_file("check_pt/model_save_" + std::to_string(i) + ".txt", std::ios::app);
-        //save_file << trainer.model;
       }
+
+      if(i !=0 && i % save_rate == 0){
+        std::fstream save_file("check_pt/model_save_" + std::to_string(i) + ".txt", std::ios::app);
+        save_file << trainer.model;
+      }
+
     }
 }
